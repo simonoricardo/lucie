1de532a788e3ad099cc03f3b0dd42cbe805d922b
Simon Ricard <ricardsimon@rumandcode.io>
2024-05-16 10:54:44 -0400

    main - Fix tests

--- config/test.exs
+++ config/test.exs
@@ -14,7 +14,8 @@ config :lucie, Lucie.Repo,
   hostname: "localhost",
   database: "lucie_test#{System.get_env("MIX_TEST_PARTITION")}",
   pool: Ecto.Adapters.SQL.Sandbox,
-  pool_size: System.schedulers_online() * 2
+  pool_size: System.schedulers_online() * 2,
+  port: 5555
 
 # We don't run a server during test. If one is required,
 # you can enable the server option below.
